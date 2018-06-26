5b9d2b36d69e31f6d3d0a9506e439768764be9be
shuhei.morioka <selmertsx@gmail.com>
2018-06-26 20:31:02 +0900

    add annotate gem

--- app/models/pomodoro_memo.rb
+++ app/models/pomodoro_memo.rb
@@ -1,2 +1,13 @@
+# == Schema Information
+#
+# Table name: pomodoro_memos
+#
+#  id               :bigint(8)        not null, primary key
+#  memo             :text(65535)      not null
+#  pomodoro_task_id :integer          not null
+#  created_at       :datetime         not null
+#  updated_at       :datetime         not null
+#
+
 class PomodoroMemo < ApplicationRecord
 end
--- app/models/pomodoro_task.rb
+++ app/models/pomodoro_task.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: pomodoro_tasks
+#
+#  id         :bigint(8)        not null, primary key
+#  objective  :string(255)      not null
+#  user_id    :integer          not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class PomodoroTask < ApplicationRecord
   belongs_to :user
 end
