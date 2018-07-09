9717e8ea1116a7b9a49e99b12e816be4aae5c319
shuhei.morioka <selmertsx@gmail.com>
2018-07-09 14:01:03 +0900

    nginx luaでhttp接続ができるようになるまで

--- docker/nginx/src/test.lua
+++ docker/nginx/src/test.lua
@@ -1,1 +1,15 @@
-ngx.say('sample!')
+local http = require "resty.http"
+local httpc = http.new()
+local res, err = httpc:request_uri("http://selmertsx.hatenablog.com/", {
+  method = "GET",
+  headers = {
+    ["Content-Type"] = "application/x-www-form-urlencoded",
+  }
+})
+
+if not res then
+  ngx.say("failed to request: ", err)
+  return
+end
+
+ngx.say(res.body)
