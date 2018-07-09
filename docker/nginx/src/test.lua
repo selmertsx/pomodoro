b0c2dbb811fa3d72cb0efa4401d7fd39e7622fe5
shuhei.morioka <selmertsx@gmail.com>
2018-07-09 18:23:06 +0900

    set base of test.lua

--- docker/nginx/src/test.lua
+++ docker/nginx/src/test.lua
@@ -1,15 +1,16 @@
-local http = require "resty.http"
-local httpc = http.new()
-local res, err = httpc:request_uri("https://www.google.co.jp/", {
-  method = "GET",
-  headers = {
-    ["Content-Type"] = "application/x-www-form-urlencoded",
-  }
-})
+local opts = {
+  redirect_uri_path = ""
+  discovery = ""
+  client_id = ""
+  client_secret = ""
+}
 
-if not res then
-  ngx.say("failed to request: ", err)
-  return
+local res, err = require("resty.openidc").authenticate(opts)
+
+if err then
+  ngx.status = 500
+  ngx.say(err)
+  ngx.exit(ngx.HTTP_INTERNAL_SERVER_ERROR)
 end
 
-ngx.say(res.body)
+ngx.req.set_header("X-USER", res.id_token.sub)
