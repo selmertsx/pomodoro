cf5f5616bb25fbde286cb13b3daddcb46f0e5ed9
shuhei.morioka <selmertsx@gmail.com>
2018-07-19 16:28:33 +0900

    be enable to build rails in local environment

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 
 ActiveRecord::Schema.define(version: 2018_06_26_111310) do
 
-  create_table "pomodoro_memos", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+  create_table "pomodoro_memos", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci", force: :cascade do |t|
     t.text "memo", null: false
     t.integer "pomodoro_task_id", null: false
     t.datetime "created_at", null: false
@@ -20,7 +20,7 @@ ActiveRecord::Schema.define(version: 2018_06_26_111310) do
     t.index ["pomodoro_task_id"], name: "index_pomodoro_memos_on_pomodoro_task_id"
   end
 
-  create_table "pomodoro_tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+  create_table "pomodoro_tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci", force: :cascade do |t|
     t.string "objective", null: false
     t.integer "user_id", null: false
     t.datetime "created_at", null: false
@@ -28,7 +28,7 @@ ActiveRecord::Schema.define(version: 2018_06_26_111310) do
     t.index ["user_id"], name: "index_pomodoro_tasks_on_user_id"
   end
 
-  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci", force: :cascade do |t|
     t.string "name", null: false
     t.string "email", null: false
     t.datetime "created_at", null: false
