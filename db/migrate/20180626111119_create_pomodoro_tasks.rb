ca28957f475ba352415c9a83df0896f1308699d8
shuhei.morioka <selmertsx@gmail.com>
2018-08-07 19:51:26 +0900

    rename table name pomodoro_tasks to tasks

--- db/migrate/20180626111119_create_pomodoro_tasks.rb
+++ db/migrate/20180626111119_create_pomodoro_tasks.rb
@@ -1,6 +1,6 @@
 class CreatePomodoroTasks < ActiveRecord::Migration[5.2]
   def change
-    create_table :pomodoro_tasks do |t|
+    create_table :tasks do |t|
       t.string :objective, null: false
       t.integer :user_id, null: false
       t.index :user_id
