75b221834cc9f237f74da2e740adb024f080b284
shuhei.morioka <selmertsx@gmail.com>
2018-08-08 15:30:58 +0900

    ignore idea

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,11 @@
 
 class HomeController < ApplicationController
+  include ActionController::RequestForgeryProtection
+  include ActionController::ImplicitRender
+  include ActionView::Layouts
+
+  layout "application"
+
   def index
   end
 end
