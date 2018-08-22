675456b1590ddbc98b8381dff11a225c5bb59efa
shuhei.morioka <selmertsx@gmail.com>
2018-08-22 19:12:18 +0900

    userのmigration fileを作成した
    userはoidcのOPから渡されるパラメータの中で、サービス内で利用する可能性のあるパラメータをカラムとして保持するようにした

--- db/migrate/20180822095358_create_users.rb
+++ db/migrate/20180822095358_create_users.rb
@@ -1,8 +1,14 @@
 class CreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
-
+      t.string :name, null: false
+      t.string :sub, null: false
+      t.string :preferred_username, null: false
       t.timestamps
     end
+
+    add_index :users, :name
+    add_index :users, :sub
+    add_index :users, :preferred_username
   end
 end
